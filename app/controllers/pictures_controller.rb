499ab6c10a4466b36640644cba3a4753d15fe775
Courtney Lee <courtneylee2113@gmail.com>
2015-04-26 22:31:31 -0400

    making each pic show at its own url

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -4,6 +4,9 @@ def index
 @pictures =Picture.all
 end
 
+def show
+	@picture =Picture.find(params[:id])
+end
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 
 Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
+  get 'pictures/:id' => 'pictures#show', as: 'picture'
+end
+
+
+
+
+
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -56,4 +65,3 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
