a589a91a02cc996738a76c07f4e02a528944ed7d
Courtney Lee <courtneylee2113@gmail.com>
2015-04-26 23:26:39 -0400

    adding features to create new pictures using html form

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,5 +8,17 @@ def show
 	@picture =Picture.find(params[:id])
 end
 
+def new 
+end
+
+  def create
+    render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+end
+
+
+
+
+
+
 end
 
